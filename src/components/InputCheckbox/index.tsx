57f502f6d228ec55eee6c8b689e1ea99bfa0c945
Giacomo Pessotto <giacomopesso@gmail.com>
2023-03-28 20:12:29 +0100

    second bug fixed

--- src/components/InputCheckbox/index.tsx
+++ src/components/InputCheckbox/index.tsx
@@ -12,14 +12,14 @@ export const InputCheckbox: InputCheckboxComponent = ({ id, checked = false, dis
           "RampInputCheckbox--label-checked": checked,
           "RampInputCheckbox--label-disabled": disabled,
         })}
+        onClick={() => onChange(!checked)}
       />
       <input
         id={inputId}
         type="checkbox"
         className="RampInputCheckbox--input"
-        checked={checked}
+        defaultChecked={checked}
         disabled={disabled}
-        onChange={() => onChange(!checked)}
       />
     </div>
   )
